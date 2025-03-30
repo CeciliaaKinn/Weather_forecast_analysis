2ea4286bb4dd3271043acf29cfb43c519b3849cf
emeliahult <emeliah@stud.ntnu.no>
2025-03-30 15:12:55 +0200

    Changing functions identidy_missing_values and find_missing_data

--- tests/DataPreparation._tester.py
+++ tests/DataPreparation._tester.py
@@ -1,20 +1,20 @@
 import sys 
 import os 
 
-
 # Find the absolute path to the project root
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 # Add the src folder to sys.path
 sys.path.append(os.path.join(project_root, 'src'))
 
-from src.services.DataPreparation import DataPreparation 
-
-from src.services.WindSpeedProcessing import WindSpeedProcessing
-from src.services.AirTemperatureProcessing import AirTemperatureProcessing 
-
+from services.DataPreparation import DataPreparation
 
 # Instantiate the class
-data_fetcher = WindSpeedProcessing()
+
+lat = '59.9423'
+lon = '10.72'
+d_from = '2024-04-01'
+d_to = '2024-06-01'
+data_fetcher = DataPreparation(lat, lon, d_from, d_to)
 
 # Elements to retrieve (monthly mean temperature, wind speed, air pressure and monthly sum precipitation)
 # Can use any number and combination of the four
@@ -24,18 +24,20 @@ elements = ['mean(air_temperature P1M)', 'mean(wind_speed P1M)', 'sum(precipitat
 referencetime = '2010-01-01/2010-12-31'  # Example
 
 # Get the data in JSON format
-data = data_fetcher.fetch_data(elements, referencetime)
+data = data_fetcher.fetch_data(lat, lon, d_from, d_to)
 
 # Display the monthly averages or sums for the requested elements
-data_fetcher.display_monthly_average(elements, referencetime)
+#data_fetcher.display_monthly_average(lat, lon, d_from, d_to)
 
 #dp = DataPreparation(api_url = 'https://frost.met.no/observations/v0.jsonld')
 #dp.data = json_data 
 
-preview_data = DataPreparation().preview_data(data)
-missing_data = DataPreparation().find_missing_data(data)
+
+preview_data = data_fetcher.preview_data(data)
+missing_data = data_fetcher.identify_missing_values()
+missing_data2 = data_fetcher.find_missing_data()
 
 # Print the results
-print(preview_data())
-print(missing_data())
-
+print(preview_data)
+print(missing_data)
+print(missing_data2)
